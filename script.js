d03b5b8a98f6dfeb8dd8470baef2c8ebf8bc35a0
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-03-21 12:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-	// var ans = str.trim();
-	var str1 = str.toLowerCase();
+	var ans = str.trim();
+	var str1 = ans.toLowerCase();
 for(var i=0; i<str1.length/2; i++){
 	if(str1[i]==str1.length-i){
 		return "true";
